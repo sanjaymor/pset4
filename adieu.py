28759ac5e04bc845c433117024d7230677f68e47
sanjaymor <opsgaming45@gmail.com>
2026-07-16 16:38:10 +0530

    Update adieu.py

--- adieu.py
+++ adieu.py
@@ -1,18 +1,13 @@
-names = []
-
+My Solution: import inflect
+p = inflect.engine()
+Ist = |]
 while True:
-    try:
-        name = input("Name: ")
-        names.append(name)
-    except EOFError:
-        print()
-        break
-
-print("Adieu, adieu, to ", end="")
-
-if len(names) == 1:
-    print(names[0])
-elif len(names) == 2:
-    print(f"{names[0]} and {names[1]}")
-else:
-    print(", ".join(names[:-1]) + ", and " + names[-1])
+try:
+name = input("Names: ")
+Ist.append (name)
+except EOFError:
+print ("\n")
+break
+for name in Ist:
+mylist = p.join ((Ist), final_sep=",")
+print (f"Adieu, adieu, to {mylist}")
