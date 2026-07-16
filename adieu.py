4ad17e73a6e36766824183cc3f6a250367c976ad
sanjaymor <opsgaming45@gmail.com>
2026-07-16 21:18:56 +0530

    Update adieu.py

--- adieu.py
+++ adieu.py
@@ -1,16 +1,28 @@
 import inflect
-import sys
 
-p = inflect.engine()
-names = []
 
-while True:
-    try:
-        name = input('Name: ')
+def main():
+    names = get_names()
+    print(f"Adieu, adieu, to {format_names(names)}")
+
+
+def get_names():
+    names = []
+
+    while True:
+        try:
+            name = input("Name: ")
+        except EOFError:
+            print()
+            return names
+
         names.append(name)
 
-    except EOFError:
-        print()
-        break
 
-print('Adieu, adieu, to ' + p.join(names))
+def format_names(names):
+    p = inflect.engine()
+    return p.join(names)
+
+
+if __name__ == "__main__":
+    main()
